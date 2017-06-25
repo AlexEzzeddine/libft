20d153bad5dad3ae53d7be074857831d7cd245ba
alex <alex.izzedin@gmail.com>
2017-06-24 21:49:24 -0700

    strndup fix

--- ft_strndup.c
+++ ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: aezzeddi <aezzeddi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/24 21:44:24 by aezzeddi          #+#    #+#             */
-/*   Updated: 2017/06/24 21:45:04 by aezzeddi         ###   ########.fr       */
+/*   Updated: 2017/06/24 21:48:58 by aezzeddi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,10 @@
 char	*ft_strndup(const char *src, size_t n)
 {
 	char	*dup;
-	int		i;
 
 	dup = ft_strnew(n);
 	if (!dup)
 		return (NULL);
-	ft_strncpy(dup, src);
+	ft_strncpy(dup, src, n);
 	return (dup);
 }
